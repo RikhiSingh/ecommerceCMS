1fe2d85a18f98228c5cfccce3c2611591f4f137b
rikhisingh <rikhisinghnov@gmail.com>
2024-04-12 17:27:23 -0400

    started dialog, authen finished

--- ecommerce-admin/app/(root)/page.tsx
+++ ecommerce-admin/app/(root)/page.tsx
@@ -1,9 +1,13 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
-export default function Home() {
+const SetupPage = () => {
   return (
     <div className="p-4">
-      <Button>Click Me</Button>
+      <UserButton afterSignOutUrl="/"/>
     </div>
   );
 }
+
+
+export default SetupPage;
