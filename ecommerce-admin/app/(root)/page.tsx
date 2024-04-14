720a2392f2aae9517edfb9eeaf97114aab98962a
rikhisingh <rikhisinghnov@gmail.com>
2024-04-14 17:04:15 -0400

    form components finished

--- ecommerce-admin/app/(root)/page.tsx
+++ ecommerce-admin/app/(root)/page.tsx
@@ -2,14 +2,23 @@
 
 
 import { Modal } from "@/components/ui/modal";
+import { useStoreModal } from "@/hooks/use-store-modal";
 import { UserButton } from "@clerk/nextjs";
+import { useEffect } from "react";
 
 const SetupPage = () => {
+  const onOpen = useStoreModal((state) => state.onOpen);
+  const isOpen = useStoreModal((state) => state.isOpen);
+
+  useEffect(() =>{
+    if(!isOpen){
+      onOpen();
+    }
+  }, [isOpen, onOpen]);
+
   return (
     <div className="p-4">
-      <Modal title="Test" description="test descriotion" isOpen onClose={()=>{}}>
-        Children
-      </Modal>
+      Root Page
     </div>
   );
 }
