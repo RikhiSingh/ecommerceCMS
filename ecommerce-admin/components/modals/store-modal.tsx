da11ff791b2f5562c5fae03347643dd8768f6390
rikhisingh <rikhisinghnov@gmail.com>
2024-04-16 11:29:06 -0400

    store name validator and zodResolver

--- ecommerce-admin/components/modals/store-modal.tsx
+++ ecommerce-admin/components/modals/store-modal.tsx
@@ -1,10 +1,23 @@
 'use client';
 
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+
 import { useStoreModal } from "@/hooks/use-store-modal";
 import { Modal } from "@/components/ui/modal";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const formSchema = z.object({
+    // atleast 1 character is required to name the store
+    name: z.string().min(1),
+})
 
 export const StoreModal = () => {
     const storeModal = useStoreModal();
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema)
+    })
 
     return (
         <Modal
