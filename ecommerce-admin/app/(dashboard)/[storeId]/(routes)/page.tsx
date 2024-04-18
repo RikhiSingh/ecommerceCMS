55b4ccaa441d6ef839548267ac99d2f6c151f3b0
rikhisingh <rikhisinghnov@gmail.com>
2024-04-18 01:44:56 -0400

    app layout finished

---
+++ ecommerce-admin/app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -0,0 +1,9 @@
+const DashboardPage = () => {
+    return ( 
+        <div>
+            This is a Dashboard!
+        </div>
+     );
+}
+ 
+export default DashboardPage;
---
+++ ecommerce-admin/app/(root)/layout.tsx
@@ -0,0 +1,33 @@
+import prismadb from "@/lib/prismadb";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+export default async function SetupLayout({
+    children
+}:{
+    children: React.ReactNode;
+}){
+    const {userId} = auth();
+
+    if(!userId){
+        redirect('/sign-in')
+    }
+
+    // load first store available
+    const store= await prismadb.store.findFirst({
+        where:{
+            userID: userId
+        }
+    });
+
+    // if the store retrived from above exists redirect us to to that [storeId] which leads to dashboard which will confirm the same that storeId exists from cuurent logged in used if it doesnt go to root else render navigation bar and children
+    if (store){
+        redirect(`/${store.id}`);
+    }
+
+    return(
+        <>
+            {children}
+        </>
+    )
+}
