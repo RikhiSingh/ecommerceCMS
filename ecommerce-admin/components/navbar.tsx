ecbfb6b22271949998716e493dd4442b86363905
rikhisingh <rikhisinghnov@gmail.com>
2024-04-18 15:55:02 -0400

    setting url done in nav

--- ecommerce-admin/components/navbar.tsx
+++ ecommerce-admin/components/navbar.tsx
@@ -1,7 +1,19 @@
+import { UserButton } from "@clerk/nextjs";
+
+import { MainNav } from "@/components/main-nav";
+
 const Navbar = () => {
     return ( 
-        <div>
-            This is a component navbar
+        <div className="border-b">
+            <div className="flex h-16 items-center px-4">
+                <div>
+                    This will be a store switcher
+                </div>
+                <MainNav className="mx-6"/>
+                <div className="ml-auto flex items-center space-x-4">
+                    <UserButton afterSignOutUrl="/"/>
+                </div>
+            </div>
         </div>
      );
 }
