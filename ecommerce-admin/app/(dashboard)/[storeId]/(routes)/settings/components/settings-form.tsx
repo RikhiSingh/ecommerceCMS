7ef0e8c46ec3033d98a3ec185726ba745b0de703
rikhisingh <rikhisinghnov@gmail.com>
2024-04-18 18:09:55 -0400

    settings form skeleton finished; next step create the form itself

---
+++ ecommerce-admin/app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import * as z from "zod";
+import { Store } from "@prisma/client";
+
+import { Heading } from "@/components/ui/heading";
+import { Button } from "@/components/ui/button";
+import { Trash } from "lucide-react";
+import { Separator } from "@/components/ui/separator";
+
+interface SettingsFormProps {
+    initialData: Store;
+}
+
+const formSchema = z.object({
+    name: z.string().min(1),
+});
+
+type SettingsFormValues = z.infer<typeof formSchema>;
+
+export const SettingsForm: React.FC<SettingsFormProps> = ({
+    initialData
+}) => {
+    return (
+        <>
+            <div className="flex items-center justify-between">
+                <Heading
+                    title="Settings"
+                    description="Manage store preferences"
+                />
+                <Button
+                    variant="destructive"
+                    size="icon"
+                    onClick={() => { }}
+                >
+                    <Trash className="h-4 w-4" />
+                </Button>
+            </div>
+            <Separator />
+        </>
+    )
+}
