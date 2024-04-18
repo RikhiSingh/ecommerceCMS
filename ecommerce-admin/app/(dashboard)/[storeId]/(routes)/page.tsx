4650ff974fa3fdac065d010c2db8898823d8bcc8
rikhisingh <rikhisinghnov@gmail.com>
2024-04-18 11:51:40 -0400

    dashboard setup (admin) completed

--- ecommerce-admin/app/(dashboard)/[storeId]/(routes)/page.tsx
+++ ecommerce-admin/app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,7 +1,22 @@
-const DashboardPage = () => {
+import prismadb from "@/lib/prismadb";
+
+interface DashboardPageProps{
+    params: { storeId: string}
+};
+
+const DashboardPage: React.FC<DashboardPageProps> = async ({
+    // destructure the params
+    params
+}) => {
+    const store = await prismadb.store.findFirst({
+        where:{
+            id: params.storeId
+        }
+    });
+
     return ( 
         <div>
-            This is a Dashboard!
+            Active Store: {store?.name};
         </div>
      );
 }
