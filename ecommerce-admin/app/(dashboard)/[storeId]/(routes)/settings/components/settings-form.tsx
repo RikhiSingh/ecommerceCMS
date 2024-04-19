eca3092870d45ac692f1068f662ae142ed45674f
rikhisingh <rikhisinghnov@gmail.com>
2024-04-19 11:11:58 -0400

    delete store alert modal finished

--- ecommerce-admin/app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ ecommerce-admin/app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -22,6 +22,7 @@ import {
     FormMessage
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { AlertModal } from "@/components/modals/alert-modal";
 
 interface SettingsFormProps {
     initialData: Store;
@@ -62,8 +63,30 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({
         }
     }
 
+    // to delete
+    const onDelete = async () =>{
+        try{
+            setLoading(true)
+            await axios.delete(`/api/stores/${params.storeId}`)
+            router.refresh();
+            router.push("/")
+            toast.success("Store deleted.");
+        } catch(error){
+            toast.error("Make sure you remove all the products and categories first.");
+        }finally{
+            setLoading(false)
+            setOpen(false)
+        }
+    }
+
     return (
         <>
+        <AlertModal 
+            isOpen={open}
+            onClose={() => setOpen(false)}
+            onConfirm={onDelete}
+            loading={loading}
+        />
             <div className="flex items-center justify-between">
                 <Heading
                     title="Settings"
