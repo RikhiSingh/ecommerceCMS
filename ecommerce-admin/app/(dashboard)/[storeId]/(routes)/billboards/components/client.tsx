6d4383b78be69ee4e8772af826f797f21ee4ebd9
rikhisingh <rikhisinghnov@gmail.com>
2024-04-20 18:13:01 -0400

    billboard count shown

--- ecommerce-admin/app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ ecommerce-admin/app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -2,12 +2,19 @@
 
 import { Plus } from "lucide-react";
 import { useParams, useRouter } from "next/navigation";
+import { Billboard } from "@prisma/client";
 
 import { Button } from "@/components/ui/button";
 import { Heading } from "@/components/ui/heading";
 import { Separator } from "@radix-ui/react-separator";
 
-export const BillboardClient = () => {
+interface BillboardClientProps{
+    data: Billboard[]
+}
+
+export const BillboardClient: React.FC<BillboardClientProps> = ({
+    data
+}) => {
 
     const router = useRouter();
     const params = useParams();
@@ -16,7 +23,7 @@ export const BillboardClient = () => {
         <>
             <div className="flex items-center justify-between">
                 <Heading
-                    title="Billboards (0)"
+                    title={`Billboards (${data.length})`}
                     description="Manage billboards for your store"
                 />
                 <Button onClick={() => router.push(`/${params.storeId}/billboards/new`)}>
