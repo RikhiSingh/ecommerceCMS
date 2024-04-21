6c243ec9a53150808e3230b2f03799474152bbc2
rikhisingh <rikhisinghnov@gmail.com>
2024-04-21 17:53:25 -0400

    delete fixed

--- ecommerce-admin/app/(dashboard)/[storeId]/(routes)/billboards/components/cell-action.tsx
+++ ecommerce-admin/app/(dashboard)/[storeId]/(routes)/billboards/components/cell-action.tsx
@@ -42,7 +42,7 @@ export const CellAction: React.FC<CellActionProps> = ({
     const onDelete = async () => {
         try {
             setLoading(true)
-            await axios.delete(`/api/${params.storeId}/billboards/${params.billboardId}`);
+            await axios.delete(`/api/${params.storeId}/billboards/${data.id}`);
             router.refresh();
             toast.success("Billboard deleted.");
         } catch (error) {
