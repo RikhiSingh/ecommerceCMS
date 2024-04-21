58cb3716522c7c1d1f19987a41a01d934009b9df
rikhisingh <rikhisinghnov@gmail.com>
2024-04-21 17:34:16 -0400

    added cell-action

--- ecommerce-admin/app/(dashboard)/[storeId]/(routes)/billboards/components/columns.tsx
+++ ecommerce-admin/app/(dashboard)/[storeId]/(routes)/billboards/components/columns.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { ColumnDef } from "@tanstack/react-table"
+import { CellAction } from "./cell-action"
 
 export type BillboardColumn = {
   id: string
@@ -17,4 +18,8 @@ export const columns: ColumnDef<BillboardColumn>[] = [
     accessorKey: "createdAt",
     header: "Date",
   },
+  {
+    id: "actions",
+    cell: ({ row }) => <CellAction data={row.original} />
+  }
 ]
