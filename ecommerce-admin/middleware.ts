fc3a34068d53236a701d2d0b1ea8b64e978306ba
rikhisingh <rikhisinghnov@gmail.com>
2024-04-21 18:17:26 -0400

    api route fixed for public (tested in insomnia)

--- ecommerce-admin/middleware.ts
+++ ecommerce-admin/middleware.ts
@@ -1,12 +1,13 @@
 import { authMiddleware } from "@clerk/nextjs";
- 
+
 // See https://clerk.com/docs/references/nextjs/auth-middleware
 // for more information about configuring your Middleware
 export default authMiddleware({
   // Allow signed out users to access the specified routes:
   // publicRoutes: ['/anyone-can-visit-this-route'],
+  publicRoutes: ["/api/:path*"],
 });
- 
+
 export const config = {
   matcher: [
     // Exclude files with a "." followed by an extension, which are typically static files.
