b203240b7d8fcd5cb8987b244037be8d6d808a34
rikhisingh <rikhisinghnov@gmail.com>
2024-04-22 12:28:46 -0400

    copied billboard as categories

---
+++ ecommerce-admin/app/(dashboard)/[storeId]/(routes)/categories/[billboardId]/components/billboard-form.tsx
@@ -0,0 +1,167 @@
+"use client";
+
+import * as z from "zod";
+import { Billboard } from "@prisma/client";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { Trash } from "lucide-react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { useParams, useRouter } from "next/navigation";
+
+import { Heading } from "@/components/ui/heading";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { AlertModal } from "@/components/modals/alert-modal";
+import { ApiAlert } from "@/components/ui/api-alert";
+import { useOrigin } from "@/hooks/use-origin";
+import ImageUpload from "@/components/ui/image-upload";
+
+const formSchema = z.object({
+    label: z.string().min(1),
+    imageUrl: z.string().min(1),
+});
+
+type BillboardFormValues = z.infer<typeof formSchema>;
+
+interface BillboardFormProps {
+    initialData: Billboard | null;
+}
+
+export const BillboardForm: React.FC<BillboardFormProps> = ({
+    initialData
+}) => {
+    const params = useParams();
+    const router = useRouter();
+    const origin = useOrigin();
+
+    // for alert modal
+    const [open, setOpen] = useState(false);
+    const [loading, setLoading] = useState(false);
+
+    const title = initialData ? "Edit Billboard" : "Create Billboard";
+    const description = initialData ? "Edit a Billboard" : "Add a new billboard";
+    const toastMessage = initialData ? "Billboard Updated" : "Billboard created";
+    const action = initialData ? "Save changes" : "Create";
+
+    const form = useForm<BillboardFormValues>({
+        resolver: zodResolver(formSchema),
+        defaultValues: initialData || {
+            label: '',
+            imageUrl: ''
+        }
+    });
+
+    const onSubmit = async (data: BillboardFormValues) => {
+        // console.log(data);
+        try {
+            setLoading(true);
+            if (initialData) {
+                await axios.patch(`/api/${params.storeId}/billboards/${params.billboardId}`, data);
+            } else {
+                await axios.post(`/api/${params.storeId}/billboards`, data);                
+            }
+            router.refresh();
+            router.push(`/${params.storeId}/billboards`)
+            toast.success(toastMessage);
+        } catch (error) {
+            toast.error("Something went wrong.Please contact network administrator");
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    // to delete
+    const onDelete = async () => {
+        try {
+            setLoading(true)
+            await axios.delete(`/api/${params.storeId}/billboards/${params.billboardId}`);
+            router.refresh();
+            router.push(`/${params.storeId}/billboards`);
+            toast.success("Billboard deleted.");
+        } catch (error) {
+            toast.error("Make sure you removed all categories using this billboard first.");
+        } finally {
+            setLoading(false)
+            setOpen(false)
+        }
+    }
+
+    return (
+        <>
+            <AlertModal
+                isOpen={open}
+                onClose={() => setOpen(false)}
+                onConfirm={onDelete}
+                loading={loading}
+            />
+            <div className="flex items-center justify-between">
+                <Heading
+                    title={title}
+                    description={description}
+                />
+                {initialData && (
+                    <Button
+                        disabled={loading}
+                        variant="destructive"
+                        size="icon"
+                        onClick={() => setOpen(true)}
+                    >
+                        <Trash className="h-4 w-4" />
+                    </Button>
+                )}
+            </div>
+            <Separator />
+            <Form {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 w-full">
+                    <FormField
+                        control={form.control}
+                        name="imageUrl"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>BackgroundImage</FormLabel>
+                                <FormControl>
+                                    <ImageUpload
+                                        value={field.value ? [field.value] : []}
+                                        disabled={loading}
+                                        onChange={(url) => field.onChange(url)}
+                                        onRemove={() => field.onChange("")}
+                                    />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <div className="grid grid-cols-3 gap-8">
+                        <FormField
+                            control={form.control}
+                            name="label"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Label</FormLabel>
+                                    <FormControl>
+                                        <Input disabled={loading} placeholder="Billboard Label" {...field} />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                    </div>
+                    <Button disabled={loading} className="ml-auto" type="submit">
+                        {action}
+                    </Button>
+                </form>
+            </Form>
+        </>
+    )
+}
--- ecommerce-admin/components/main-nav.tsx
+++ ecommerce-admin/components/main-nav.tsx
@@ -19,14 +19,14 @@ export function MainNav({
             active: pathname === `/${params.storeId}`,
         },
         {
-            href: `/${params.storeId}/categories`,
-            label: 'Categories',
-            active: pathname === `/${params.storeId}/categories`,
-        },
-        {
             href: `/${params.storeId}/billboards`,
             label: 'Billboards',
             active: pathname === `/${params.storeId}/billboards`,
+        },
+        {
+            href: `/${params.storeId}/categories`,
+            label: 'Categories',
+            active: pathname === `/${params.storeId}/categories`,
         },
         {
             href: `/${params.storeId}/settings`,
