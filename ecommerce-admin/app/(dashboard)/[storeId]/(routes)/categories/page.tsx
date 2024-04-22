d1e995f1f6195174567bb281b2faaa349f967066
rikhisingh <rikhisinghnov@gmail.com>
2024-04-22 12:41:50 -0400

    category page finished along with CategoryColumn

--- ecommerce-admin/app/(dashboard)/[storeId]/(routes)/categories/page.tsx
+++ ecommerce-admin/app/(dashboard)/[storeId]/(routes)/categories/page.tsx
@@ -3,32 +3,37 @@ import { format } from "date-fns";
 import prismadb from "@/lib/prismadb";
 
 import { BillboardClient } from "./components/client";
-import { BillboardColumn } from "./components/columns";
+import { CategoryColumn } from "./components/columns";
 
 const CategoriesPage = async ({
     params
 }: {
     params: { storeId: string }
 }) => {
-    const billboards = await prismadb.billboard.findMany({
+    const categories = await prismadb.category.findMany({
         where: {
             storeId: params.storeId
+        },
+        // which billboard is category gonna be realted to
+        include:{
+            billboard: true,
         },
         orderBy: {
             createdAt: 'desc'
         }
     })
 
-    const formattedBillboards: BillboardColumn[] = billboards.map((item) => ({
+    const formattedCategories: CategoryColumn[] = categories.map((item) => ({
         id: item.id,
-        label: item.label,
+        name: item.name,
+        billboardLabel: item.billboard.label,
         createdAt: format(item.createdAt, "MMMM do, yyyy")
     }));
 
     return (
         <div className="flex-col">
             <div className="flex-1 space-y-4 p-8 pt-6">
-                <BillboardClient data={formattedBillboards} />
+                <BillboardClient data={formattedCategories} />
             </div>
         </div>
     );
