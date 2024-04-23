26284da76a1b298b14e614791252bb41fc1152d9
rikhisingh <rikhisinghnov@gmail.com>
2024-04-23 17:52:53 -0400

    products link added to the main-nav

--- ecommerce-admin/components/main-nav.tsx
+++ ecommerce-admin/components/main-nav.tsx
@@ -39,6 +39,11 @@ export function MainNav({
             active: pathname === `/${params.storeId}/colors`,
         },
         {
+            href: `/${params.storeId}/products`,
+            label: 'Products',
+            active: pathname === `/${params.storeId}/products`,
+        },
+        {
             href: `/${params.storeId}/settings`,
             // make the the setting that get loaded are only for that storeId
             label: 'Settings',
