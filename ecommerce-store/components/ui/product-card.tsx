010bc8ad88bfd8f76043b095cdf19ac7f96756fa
rikhisingh <rikhisinghnov@gmail.com>
2024-04-24 18:49:02 -0400

    onclick for individual product on main page made

--- ecommerce-store/components/ui/product-card.tsx
+++ ecommerce-store/components/ui/product-card.tsx
@@ -6,6 +6,7 @@ import { Expand, ShoppingCart } from "lucide-react";
 import { Product } from "@/types";
 import IconButton from "@/components/ui/icon-button";
 import Currency from "@/components/ui/currency";
+import { useRouter } from "next/navigation";
 
 interface ProductCard {
     data: Product;
@@ -14,8 +15,14 @@ interface ProductCard {
 const ProductCard: React.FC<ProductCard> = ({
     data
 }) => {
+    const router = useRouter();
+
+    const handleClick = () =>{
+        router.push(`/products/${data?.id}`);
+    }
+
     return (
-        <div className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
+        <div onClick={handleClick} className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
             {/* Images and actions */}
             <div className="aspect-square rounded-xl bg-gray-100 relative">
                 <Image
