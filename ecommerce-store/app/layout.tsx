732c7c70dbcec04c2d678183ba72ee4c51e63bdf
rikhisingh <rikhisinghnov@gmail.com>
2024-04-24 11:16:50 -0400

    navbar finished

--- ecommerce-store/app/layout.tsx
+++ ecommerce-store/app/layout.tsx
@@ -3,6 +3,7 @@ import { Urbanist } from "next/font/google";
 
 import "./globals.css";
 import Footer from "@/components/footer";
+import Navbar from "@/components/navbar";
 
 const font = Urbanist({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
+        <Navbar />
         {children}
         <Footer />
         </body>
---
+++ ecommerce-store/components/navbar.tsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+    return ( 
+        <div className="border-b">
+            Navbar!
+        </div>
+     );
+}
+ 
+export default Navbar;
