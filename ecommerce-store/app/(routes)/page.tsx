e4212b03303f02a736f06906d727c2c3ecd29ed2
rikhisingh <rikhisinghnov@gmail.com>
2024-04-24 16:34:24 -0400

    billboard finished

--- ecommerce-store/app/(routes)/page.tsx
+++ ecommerce-store/app/(routes)/page.tsx
@@ -1,8 +1,18 @@
-const HomePage = () => {
+import getBillboard from "@/actions/get-billboard";
+import Container from "@/components/ui/container"
+import Billboard from "@/components/billboard";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+    const billboard = await getBillboard("1aaa49cc-b8ab-4bd5-a528-316bedeba31f");
+
     return ( 
-        <div>
-            Hello Store
-        </div>
+        <Container>
+            <div className="space-y-10 pb-10">
+                <Billboard data={billboard}/>
+            </div>
+        </Container>
      );
 }
  
