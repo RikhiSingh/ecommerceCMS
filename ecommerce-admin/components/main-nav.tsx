66ad255aaae785ee5bc1dbbacda2bcc6675df8fe
rikhisingh <rikhisinghnov@gmail.com>
2024-04-24 10:20:15 -0400

    orders added to the main-nav

--- ecommerce-admin/components/main-nav.tsx
+++ ecommerce-admin/components/main-nav.tsx
@@ -44,6 +44,11 @@ export function MainNav({
             active: pathname === `/${params.storeId}/products`,
         },
         {
+            href: `/${params.storeId}/orders`,
+            label: 'Orders',
+            active: pathname === `/${params.storeId}/orders`,
+        },
+        {
             href: `/${params.storeId}/settings`,
             // make the the setting that get loaded are only for that storeId
             label: 'Settings',
