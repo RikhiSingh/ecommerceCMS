393204e9b7c5789d6193a70a9ac9e15b3d92c84d
rikhisingh <rikhisinghnov@gmail.com>
2024-04-25 20:20:53 -0400

    interface finished

--- ecommerce-store/hooks/use-preview-modal.tsx
+++ ecommerce-store/hooks/use-preview-modal.tsx
@@ -2,3 +2,10 @@ import { create } from "zustand";
 
 import {Product} from "@/types";
 
+interface PreviewModalStore{
+    isOpen: boolean;
+    data?: Product;
+    onOpen: (data: Product) => void;
+    onClose: () => void;
+};
+
