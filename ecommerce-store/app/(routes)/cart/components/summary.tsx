e55d009032aaa780162017dd80c634f4edd03c4e
rikhisingh <rikhisinghnov@gmail.com>
2024-04-26 14:53:08 -0400

    summary base finished

--- ecommerce-store/app/(routes)/cart/components/summary.tsx
+++ ecommerce-store/app/(routes)/cart/components/summary.tsx
@@ -2,7 +2,7 @@
 
 import axios from "axios";
 import { useEffect } from "react";
-import { useSearchParams } from "next/navigation";  
+import { useSearchParams } from "next/navigation";
 import { toast } from "react-hot-toast";
 
 import Button from "@/components/ui/button";
@@ -10,9 +10,34 @@ import Currency from "@/components/ui/currency";
 import useCart from "@/hooks/use-cart";
 
 const Summary = () => {
-    return(
-        <div>
-            Summary
+    return (
+        <div
+            className="
+                mt-16
+                rounded-lg
+                bg-gray-50
+                px-4
+                py-6
+                sm:p-6
+                lg:col-span-5
+                lg:mt-0
+                lg:p-8
+            "
+        >
+            <h2 className="text-lg font-medium text-gray-900">
+                Order Summary
+            </h2>
+            <div className="mt-6 space-y-4">
+                <div className="flex items-center justify-between border-t border-gray-200 pt-4">
+                    <div className="text-base font-medium text-gray-900">
+                        Order Total
+                    </div>
+                    <Currency value={45}/>
+                </div>
+            </div>
+            <Button className="w-full mt-6">
+                Checkout
+            </Button>
         </div>
     )
 };
