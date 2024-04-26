3bd8f32fd898044f813b0da6720ed37cad9e4f78
rikhisingh <rikhisinghnov@gmail.com>
2024-04-25 20:48:39 -0400

    modal-provider hydration solved

--- ecommerce-store/providers/modal-provider.tsx
+++ ecommerce-store/providers/modal-provider.tsx
@@ -1,8 +1,21 @@
 "use client";
 
+import { useEffect, useState } from "react";
+
 import PreviewModal from "@/components/preview-modal";
 
 const ModalProvider = () => {
+    const [isMounted, setIsMounted] = useState(false);
+
+    useEffect(()=>{
+        setIsMounted(true);
+    },[]);
+
+    // ofc if we are not mounted return null (prevent hydration error)
+    if(!isMounted){
+        return null;
+    }
+
     return ( 
         <>
             <PreviewModal />
