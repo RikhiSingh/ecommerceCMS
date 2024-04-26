1f3a0689c99c5a3613fb4fb5baf0ffb3eead63cf
rikhisingh <rikhisinghnov@gmail.com>
2024-04-25 20:46:58 -0400

    modal-provider created

---
+++ ecommerce-store/providers/modal-provider.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import PreviewModal from "@/components/preview-modal";
+
+const ModalProvider = () => {
+    return ( 
+        <>
+            <PreviewModal />
+        </>
+     );
+}
+ 
+export default ModalProvider;
