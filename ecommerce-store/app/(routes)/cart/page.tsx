2a8a28e035a2863562c04403ba24bb83c7dbaf14
rikhisingh <rikhisinghnov@gmail.com>
2024-04-25 22:29:27 -0400

    page for cart created

---
+++ ecommerce-store/app/(routes)/cart/page.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+import Container from "@/components/ui/container";
+import useCart from "@/hooks/use-cart";
+
+const CartPage = () => {
+    const cart = useCart();
+    
+    return ( 
+        <div className="bg-white">
+            <Container>
+                <div className="px-4 py-16 sm:px-6 lg:px-8">
+                    <h1 className="text-3xl font-bold text-black">Shopping Cart</h1>
+                    <div className="mt-12 lg:grid lg:grid-col-12 lg:items-start gap-x-12">
+                        <div className="lg:col-span-7">
+                            {cart.items.length === 0 && <p className="text-neutral-500">No items added to the Cart.</p>}
+                            <ul>
+                                {cart.items.map((item)=>(
+                                    <CartItem 
+                                        key={item.id}
+                                        data={item}
+                                    />
+                                ))}
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </Container>
+        </div>
+     );
+}
+ 
+export default CartPage;
