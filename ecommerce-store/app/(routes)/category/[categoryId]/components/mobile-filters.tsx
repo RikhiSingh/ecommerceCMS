aed928c869e2739a35f46b12b20de0fcdee9b3f4
rikhisingh <rikhisinghnov@gmail.com>
2024-04-25 20:17:38 -0400

    category screen finished (mobile filters finished)

--- ecommerce-store/app/(routes)/category/[categoryId]/components/mobile-filters.tsx
+++ ecommerce-store/app/(routes)/category/[categoryId]/components/mobile-filters.tsx
@@ -2,12 +2,14 @@
 
 import { useState } from "react";
 import { Dialog } from "@headlessui/react";
-import { Plus } from "lucide-react";
+import { Plus, X } from "lucide-react";
 
 import { Color, Size } from "@/types";
 import Button from "@/components/ui/button";
+import IconButton from "@/components/ui/icon-button";
+import Filter from "./filter";
 
-interface MobileFiltersProps{
+interface MobileFiltersProps {
     sizes: Size[];
     colors: Color[];
 };
@@ -17,15 +19,15 @@ const MobileFilters: React.FC<MobileFiltersProps> = ({
     colors
 }) => {
     const [open, setOpen] = useState(false);
-    
+
     const onOpen = () => setOpen(true);
     const onClose = () => setOpen(false);
 
-    return ( 
+    return (
         <>
             <Button onClick={onOpen} className="flex items-center gap-x-2 lg:hidden">
                 Filters
-                <Plus size={20}/>
+                <Plus size={20} />
             </Button>
 
             <Dialog open={open} as="div" className="relative z-40 lg:hidden" onClose={onClose}>
@@ -33,9 +35,33 @@ const MobileFilters: React.FC<MobileFiltersProps> = ({
                 <div className="fixed inset-0 bg-black bg-opacity-25" />
 
                 {/* Dialog position */}
+                <div className="fixed inset-0 z-40 flex">
+                    <Dialog.Panel className="relative ml-auto flex h-full w-full max-w-xs flex-col overflow-y-auto bg-white py-4 pb-6 shadow-xl">
+                        {/* Close Button */}
+                        <div className="flex items-center justify-end px-4">
+                            <IconButton icon={<X size={15} />} onClick={onClose} />
+                        </div>
+
+                        {/* Render the filters */}
+                        <div className="p-4">
+                            <Filter
+                                valueKey="sizeId"
+                                name="Sizes"
+                                data={sizes}
+                            />
+                            <Filter
+                                valueKey="colorId"
+                                name="Colors"
+                                data={colors}
+                            />
+                        </div>
+
+                    </Dialog.Panel>
+                </div>
+
             </Dialog>
         </>
-     );
+    );
 }
- 
+
 export default MobileFilters;
