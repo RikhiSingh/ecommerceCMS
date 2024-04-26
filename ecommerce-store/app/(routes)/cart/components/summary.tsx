211d07996c64027fd4fc169065170d36d1750ff9
rikhisingh <rikhisinghnov@gmail.com>
2024-04-26 14:00:57 -0400

    necessary imports for summary

--- ecommerce-store/app/(routes)/cart/components/summary.tsx
+++ ecommerce-store/app/(routes)/cart/components/summary.tsx
@@ -1,1 +1,10 @@
 "use client";
+
+import axios from "axios";
+import { useEffect } from "react";
+import { useSearchParams } from "next/navigation";  
+import { toast } from "react-hot-toast";
+
+import Button from "@/components/ui/button";
+import Currency from "@/components/ui/currency";
+import useCart from "@/hooks/use-cart";
