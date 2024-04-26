03e26dcf379ceca05168d7f328591278ebe3a1cb
rikhisingh <rikhisinghnov@gmail.com>
2024-04-25 20:55:00 -0400

    product preview finished

--- ecommerce-store/app/layout.tsx
+++ ecommerce-store/app/layout.tsx
@@ -2,8 +2,10 @@ import type { Metadata } from "next";
 import { Urbanist } from "next/font/google";
 
 import "./globals.css";
+
 import Footer from "@/components/footer";
 import Navbar from "@/components/navbar";
+import ModalProvider from "@/providers/modal-provider";
 
 const font = Urbanist({ subsets: ["latin"] });
 
@@ -20,6 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
+        <ModalProvider />
         <Navbar />
         {children}
         <Footer />
--- ecommerce-store/components/ui/product-card.tsx
+++ ecommerce-store/components/ui/product-card.tsx
@@ -1,12 +1,14 @@
 "use client";
 
 import Image from "next/image";
+import { MouseEventHandler } from "react";
 import { Expand, ShoppingCart } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 import { Product } from "@/types";
 import IconButton from "@/components/ui/icon-button";
 import Currency from "@/components/ui/currency";
-import { useRouter } from "next/navigation";
+import usePreviewModal from "@/hooks/use-preview-modal";
 
 interface ProductCard {
     data: Product;
@@ -15,10 +17,18 @@ interface ProductCard {
 const ProductCard: React.FC<ProductCard> = ({
     data
 }) => {
+    const previewModal = usePreviewModal();
     const router = useRouter();
 
     const handleClick = () =>{
         router.push(`/product/${data?.id}`);
+    }
+
+    const onPreview: MouseEventHandler<HTMLButtonElement> = (event) =>{
+        // this stopProgration will make sure and overwrite that fact that the div returned (item card) has an onClick
+        event.stopPropagation();
+
+        previewModal.onOpen(data);
     }
 
     return (
@@ -34,7 +44,7 @@ const ProductCard: React.FC<ProductCard> = ({
                 <div className="opacity-0 group-hover:opacity-100 transition absolute w-full px-6 bottom-5">
                     <div className="flex gap-x-6 justify-center">
                         <IconButton
-                            onClick={() => { }}
+                            onClick={onPreview}
                             icon={<Expand size={20} className="text-gray-600" />}
                         />
                         <IconButton
