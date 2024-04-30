61b8718c6e9681502a065a836db7db5dd3de10e8
rikhisingh <rikhisinghnov@gmail.com>
2024-04-30 19:10:38 -0400

    admin dashboard started

--- ecommerce-admin/app/(dashboard)/[storeId]/(routes)/page.tsx
+++ ecommerce-admin/app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,24 +1,26 @@
+import { Heading } from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
 import prismadb from "@/lib/prismadb";
 
-interface DashboardPageProps{
-    params: { storeId: string}
+interface DashboardPageProps {
+    params: { storeId: string }
 };
 
 const DashboardPage: React.FC<DashboardPageProps> = async ({
     // destructure the params
     params
 }) => {
-    const store = await prismadb.store.findFirst({
-        where:{
-            id: params.storeId
-        }
-    });
-
-    return ( 
-        <div>
-            Active Store: {store?.name};
+    return (
+        <div className="flex-col">
+            <div className="flex-1 space-y-4 p-8 pt-6">
+                <Heading title="Dashboard" description="Overview of your Store"/>
+                <Separator />
+                <div className=" grid gap-4 grid-cols-3">
+                    
+                </div>
+            </div>
         </div>
-     );
+    );
 }
- 
+
 export default DashboardPage;
