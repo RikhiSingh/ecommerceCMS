e72daeb3fa359bfe4dbed4b93fd8a1d6f7ffa5d0
rikhisingh <rikhisinghnov@gmail.com>
2024-04-30 19:17:32 -0400

    dashboard cards added

--- ecommerce-admin/app/(dashboard)/[storeId]/(routes)/page.tsx
+++ ecommerce-admin/app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,6 +1,10 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+
+import { CreditCard, DollarSign, Package } from "lucide-react";
 import { Heading } from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
 import prismadb from "@/lib/prismadb";
+import { formatter } from "@/lib/utils";
 
 interface DashboardPageProps {
     params: { storeId: string }
@@ -10,13 +14,55 @@ const DashboardPage: React.FC<DashboardPageProps> = async ({
     // destructure the params
     params
 }) => {
+    const totalRevenue = await getTotalRevenue(params.storeId);
+    const salesCount = () => { };
+    const stockCount = () => { };
+
     return (
         <div className="flex-col">
             <div className="flex-1 space-y-4 p-8 pt-6">
-                <Heading title="Dashboard" description="Overview of your Store"/>
+                <Heading title="Dashboard" description="Overview of your Store" />
                 <Separator />
                 <div className=" grid gap-4 grid-cols-3">
-                    
+                    <Card>
+                        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                            <CardTitle className="text-sm font-medium">
+                                Total Revenue
+                            </CardTitle>
+                            <DollarSign className="h-4 w-4 text-muted-foreground" />
+                        </CardHeader>
+                        <CardContent>
+                            <div className="text-2xl font-bold">
+                                {formatter.format(100)}
+                            </div>
+                        </CardContent>
+                    </Card>
+                    <Card>
+                        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                            <CardTitle className="text-sm font-medium">
+                                Sales
+                            </CardTitle>
+                            <CreditCard className="h-4 w-4 text-muted-foreground" />
+                        </CardHeader>
+                        <CardContent>
+                            <div className="text-2xl font-bold">
+                                +25
+                            </div>
+                        </CardContent>
+                    </Card>
+                    <Card>
+                        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                            <CardTitle className="text-sm font-medium">
+                                Products in Stock
+                            </CardTitle>
+                            <Package className="h-4 w-4 text-muted-foreground" />
+                        </CardHeader>
+                        <CardContent>
+                            <div className="text-2xl font-bold">
+                                12
+                            </div>
+                        </CardContent>
+                    </Card>
                 </div>
             </div>
         </div>
