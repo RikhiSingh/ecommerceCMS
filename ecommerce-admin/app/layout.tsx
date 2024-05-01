44c3abfbb6393b5fdf9fb10fb769a4935ed1d03f
rikhisingh <rikhisinghnov@gmail.com>
2024-04-30 20:09:33 -0400

    dark and light mode finished

--- ecommerce-admin/app/layout.tsx
+++ ecommerce-admin/app/layout.tsx
@@ -7,6 +7,7 @@ import { ToasterProvider } from "@/providers/toast-provider";
 
 
 import "./globals.css";
+import { ThemeProvider } from "@/providers/theme-provider";
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -25,9 +26,15 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
-          <ToasterProvider />
-          <ModalProvider />
-          {children}
+          <ThemeProvider attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <ToasterProvider />
+            <ModalProvider />
+            {children}
+          </ThemeProvider>
         </body>
       </html>
     </ClerkProvider>
