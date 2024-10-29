a77eb353d2d8acd668ba720cd367484287aded4f
Rikhi Singh <114336052+RikhiSingh@users.noreply.github.com>
2024-10-29 18:35:27 -0400

    installed nextjs cors and added frontend store to it (for now allow on all routes)

--- ecommerce-admin/next.config.mjs
+++ ecommerce-admin/next.config.mjs
@@ -1,11 +1,34 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images:{
-        domains:[
-            "res.cloudinary.com",
-            "openweathermap.org",
-        ]
-    }
-};
-
-export default nextConfig;
+    images: {
+      domains: [
+        "res.cloudinary.com",
+        "openweathermap.org",
+      ],
+    },
+    async headers() {
+      return [
+        {
+          // Apply CORS headers to all routes for now
+          source: '/(.*)',
+          headers: [
+            {
+              key: 'Access-Control-Allow-Origin',
+              value: 'https://communocart-store.vercel.app',
+            },
+            {
+              key: 'Access-Control-Allow-Methods',
+              value: 'GET, POST, OPTIONS',
+            },
+            {
+              key: 'Access-Control-Allow-Headers',
+              value: 'Content-Type',
+            },
+          ],
+        },
+      ];
+    },
+  };
+  
+  export default nextConfig;
+  
