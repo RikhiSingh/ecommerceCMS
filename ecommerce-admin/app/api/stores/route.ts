21b4c18ede63306c2c46c28456b39c20af456f28
Rikhi Singh <114336052+RikhiSingh@users.noreply.github.com>
2024-10-29 16:27:46 -0400

    removed cors

--- ecommerce-admin/app/api/stores/route.ts
+++ ecommerce-admin/app/api/stores/route.ts
@@ -1,4 +1,4 @@
-import { cors } from "@/lib/init-middleware";
+// import { cors } from "@/lib/init-middleware";
 import { NextResponse } from "next/server";
 import { auth } from "@clerk/nextjs";
 
@@ -6,9 +6,9 @@ import prismadb from "@/lib/prismadb";
 
 export async function POST(
     req: Request,
-    res: Response
+    // res: Response
 ) {
-    await cors(req, res);
+    // await cors(req, res);
     try {
         const { userId } = auth();
         const body = await req.json();
--- ecommerce-admin/middleware.ts
+++ ecommerce-admin/middleware.ts
@@ -12,8 +12,7 @@ export const config = {
   matcher: [
     // Exclude files with a "." followed by an extension, which are typically static files.
     // Exclude files in the _next directory, which are Next.js internals.
-    "/((?!.+\\.[\\w]+$|_next).*)",
+    "/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"
     // Re-include any files in the api or trpc folders that might have an extension
-    "/(api|trpc)(.*)"
   ]
 };
